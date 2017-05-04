0f4bb6a2cea238cb2f5fc587bec07e81511c1b1f
Scott Stadt <scott.stadt@gmail.com>
2017-05-04 14:20:17 -0500

    finished demo

---
+++ src/icon.vue
@@ -0,0 +1,25 @@
+<template>
+  <svg :height="size" :width="size">
+    <use xmlns:xlink="http://www.w3.org/1999/xlink" :xlink:href="iconId"></use>
+  </svg>
+</template>
+
+<script>
+  export default {
+    props: {
+      name: {
+        type: String,
+        required: true
+      },
+      size: {
+        type: Number,
+        default: 20
+      }
+    },
+    computed: {
+      iconId() {
+        return `#icon-${this.name}`;
+      }
+    }
+  }
+</script>
--- src/main.js
+++ src/main.js
@@ -1,7 +1,8 @@
 import Vue from 'vue'
-import App from './App.vue'
+import iconComponent from './icon.vue'
+
+Vue.component('icon', iconComponent);
 
 new Vue({
-  el: '#app',
-  render: h => h(App)
+  el: '#app'
 })
